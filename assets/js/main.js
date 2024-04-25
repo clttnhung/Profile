4452ca8589ac80f9043fa05526da398b21457047
clttnhung <caolethituyetnhung@gmail.com>
2024-04-26 01:11:58 +0700

    upload source code

---
+++ assets/js/main.js
@@ -0,0 +1,86 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle')
+      navClose = document.getElementById('nav-close')
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    }
+)
+}
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills_header');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+    for (let i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills);
+});
+
+
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if(this.scrollY >=80 ) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if (window.scrollY >= 560) 
+        scrollUp.classList.add('show-scroll');
+    else 
+        scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollUp);
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.querySelector('.change-theme');
+const body = document.body;
+
+themeButton.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+  if (body.classList.contains('dark-theme')) {
+    themeButton.classList.remove('fa-moon');
+    themeButton.classList.add('fa-sun');
+  } else {
+    themeButton.classList.remove('fa-sun');
+    themeButton.classList.add('fa-moon');
+  }
+});
